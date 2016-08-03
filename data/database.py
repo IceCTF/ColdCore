c7e3698f6b2ae7f4ac053047cd7d1018c1e0d535
James Sigurðarson <jamiees2@gmail.com>
2016-08-03 23:08:12 +0000

    flake8

--- data/database.py
+++ data/database.py
@@ -1,6 +1,9 @@
-from peewee import *
+from peewee import PostgresqlDatabase, SqliteDatabase
+from peewee import Model, CharField, TextField, IntegerField, BooleanField, DateTimeField, ForeignKeyField, CompositeKey
+
 import bcrypt
 import config
+
 if config.production:
     db = PostgresqlDatabase(config.database.database, user=config.database.user, password=config.database.password)
 else:
@@ -11,9 +14,10 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Team(BaseModel):
     name = CharField(unique=True)
-    affiliation = CharField(null = True)
+    affiliation = CharField(null=True)
     restricts = TextField(default="")
     key = CharField(unique=True, index=True)
 
@@ -29,22 +33,23 @@ class Team(BaseModel):
         adjust_points = sum([i.value for i in self.adjustments])
         return challenge_points + adjust_points
 
+
 class User(BaseModel):
     username = CharField(unique=True, index=True)
     email = CharField(index=True)
     email_confirmed = BooleanField(default=False)
     email_confirmation_key = CharField()
-    password = CharField(null = True)
+    password = CharField(null=True)
     background = CharField()
     country = CharField()
-    tshirt_size = CharField(null = True)
-    gender = CharField(null = True)
+    tshirt_size = CharField(null=True)
+    gender = CharField(null=True)
     first_login = BooleanField(default=True)
     restricts = TextField(default="")
     team = ForeignKeyField(Team, related_name="members")
     banned = BooleanField(default=False)
-    password_reset_token = CharField(null = True)
-    password_reset_expired = DateTimeField(null = True)
+    password_reset_token = CharField(null=True)
+    password_reset_expired = DateTimeField(null=True)
 
     def set_password(self, pw):
         self.password = bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt())
@@ -56,10 +61,12 @@ class User(BaseModel):
     def eligible(self):
         return self.country == "ISL"
 
+
 class UserAccess(BaseModel):
     user = ForeignKeyField(User, related_name='accesses')
     ip = CharField()
     time = DateTimeField()
+
 
 class Challenge(BaseModel):
     name = CharField()
@@ -71,6 +78,7 @@ class Challenge(BaseModel):
     enabled = BooleanField(default=True)
     flag = TextField()
 
+
 class ChallengeSolve(BaseModel):
     user = ForeignKeyField(User, related_name='solves')
     team = ForeignKeyField(Team, related_name='solves')
@@ -80,6 +88,7 @@ class ChallengeSolve(BaseModel):
     class Meta:
         primary_key = CompositeKey('team', 'challenge')
 
+
 class ChallengeFailure(BaseModel):
     user = ForeignKeyField(User, related_name='failures')
     team = ForeignKeyField(Team, related_name='failures')
@@ -87,9 +96,11 @@ class ChallengeFailure(BaseModel):
     attempt = CharField()
     time = DateTimeField()
 
+
 class NewsItem(BaseModel):
     summary = CharField()
     description = TextField()
+
 
 class TroubleTicket(BaseModel):
     team = ForeignKeyField(Team, related_name='tickets')
@@ -98,25 +109,30 @@ class TroubleTicket(BaseModel):
     active = BooleanField(default=True)
     opened_at = DateTimeField()
 
+
 class TicketComment(BaseModel):
     ticket = ForeignKeyField(TroubleTicket, related_name='comments')
     comment_by = CharField()
     comment = TextField()
     time = DateTimeField()
 
+
 class Notification(BaseModel):
     team = ForeignKeyField(Team, related_name='notifications')
     notification = TextField()
+
 
 class ScoreAdjustment(BaseModel):
     team = ForeignKeyField(Team, related_name='adjustments')
     value = IntegerField()
     reason = TextField()
 
+
 class AdminUser(BaseModel):
     username = CharField()
     password = CharField()
     secret = CharField()
+
 
 class SshAccount(BaseModel):
     team = ForeignKeyField(Team, null=True, related_name='ssh_account')
