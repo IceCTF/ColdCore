02e3b73a8f978d8a3694ae15a033524447a30212
James Sigurðarson <jamiees2@gmail.com>
2016-08-12 14:49:25 +0100

    fix tickets

--- data/ticket.py
+++ data/ticket.py
@@ -16,10 +16,10 @@ def get_comments(ticket):
     return TicketComment.select().where(TicketComment.ticket == ticket).order_by(TicketComment.time)
 
 def create_ticket(team, summary, description):
-    return TroubleTicket.create(team=g.team, summary=summary, description=description, opened_at=datetime.now())
+    return TroubleTicket.create(team=team, summary=summary, description=description, opened_at=datetime.now())
 
 def create_comment(ticket, user, comment):
-    TicketComment.create(ticket=ticket, comment_by=user.username, comment=request.form["comment"], time=datetime.now())
+    TicketComment.create(ticket=ticket, comment_by=user.username, comment=comment, time=datetime.now())
 
 def open_ticket(ticket):
     ticket.active = True
