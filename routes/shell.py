fedf5345982cff579c635495a6f19c9b419a6953
James Sigurðarson <jamiees2@gmail.com>
2016-08-12 16:42:26 +0100

    restrict shell

--- routes/shell.py
+++ routes/shell.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, g, render_template
 
 from data import ssh
+import decorators
 
 
 shell = Blueprint("shell", __name__, template_folder="../templates/shell")
 
 
 @shell.route('/shell/')
+@decorators.must_be_allowed_to("access shell")
+@decorators.competition_running_required
+@decorators.confirmed_email_required
 def index():
     account = ssh.get_team_account(g.team)
     return render_template("shell.html", account=account)
