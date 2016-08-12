a14d87910a62decbe8725e781afa03c86a8d7c23
James Sigurðarson <jamiees2@gmail.com>
2016-08-12 21:29:02 +0100

    update caching daemon

--- daemons/cache_score.py
+++ daemons/cache_score.py
@@ -2,14 +2,15 @@ import redis
 from data import scoreboard
 import json
 import config
-
-r = redis.StrictRedis(host=config.redis.host, port=config.redis.port, db=config.redis.db)
-
-def set_complex(key, val):
-    r.set(key, json.dumps(val))
+from data.database import db
 
 
 def run():
+    r = redis.StrictRedis(host=config.redis.host, port=config.redis.port, db=config.redis.db)
+    db.connect()
+
+    def set_complex(key, val):
+        r.set(key, json.dumps(val))
     data = scoreboard.calculate_scores()
     graphdata = scoreboard.calculate_graph(data)
     set_complex("scoreboard", data)
