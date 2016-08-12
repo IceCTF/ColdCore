f7c437011d99175c76137c795007d3c47f88ceac
James Sigurðarson <jamiees2@gmail.com>
2016-08-12 16:44:07 +0100

    fix

--- routes/shell.py
+++ routes/shell.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, g, render_template
 
 from data import ssh
-import decorators
+from utils import decorators
 
 
 shell = Blueprint("shell", __name__, template_folder="../templates/shell")
